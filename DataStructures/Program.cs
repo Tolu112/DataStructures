9a1f3bb49011321325f8f79d2fc401c4c1ec5ee7
Tolu <tsorinmade@yahoo.com>
2022-07-28 21:38:08 -0700

    DataStructures

---
+++ DataStructures/Program.cs
@@ -0,0 +1,54 @@
+﻿//DATA STRUCTURES
+
+
+Console.WriteLine("Welcome to the Data Structures Database\n");
+string input = "";
+
+
+//Program loop
+bool runProgram = true;
+while (runProgram)
+{
+    //Get user input and validate entry
+    for (int numOfTries = 0; numOfTries == 0 || !Validator.Validator.ContainsOnlyLetters(input); numOfTries++)
+    {
+        var displayMessage = numOfTries > 0
+                             ? "Invalid entry. Please enter a word or sentence."
+                             : "Please enter a word you would like to reverse: ";
+        Console.Write(displayMessage);
+        input = Console.ReadLine();
+
+    }
+    //Add users word/sentence to a string array
+    string[] sentence = input.Split(" ");
+
+    //Display word/sentence in reverse
+    Array.ForEach(sentence, word => Console.Write(string.Join(ReverseString(word), " ")));
+
+    //Ask user to continue
+    runProgram = Validator.Validator.GetContinue();
+}
+
+
+
+//Method to add string to stack and reverse output
+static string ReverseString(string input)
+{
+    //Store letters of each word in character array
+    char[] letters;
+    letters = input.ToCharArray(0, input.Length);
+
+    //Instantiate Stack
+    Stack<char> inputStack = new Stack<char>();
+
+    //Add letters to stack
+    Array.ForEach(letters, l => inputStack.Push(l));
+
+    //display top element in stack
+    Array.ForEach(letters, l => Console.Write(inputStack.Pop()));
+    return input;
+}
+
+
+
+
---
+++ DataStructures/Validator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    internal class Validator
+    {
+
+        //METHODS
+
+        //Check if string contains characters or spaces
+        public static bool ContainsOnlyLetters(string str)
+            => str.All(c => char.IsLetter(c) || char.IsWhiteSpace(c));
+
+        //Check if user wishes to continue
+        public static bool GetContinue()
+        {
+            //default value
+            bool result = true;
+            //check if user wants to continue
+            while (true)
+            {
+                Console.Write("\nWould you like to continue? y/n: ");
+                string choice = Console.ReadLine().Trim().ToLower();
+
+                //Check user input
+                if(choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if(choice == "n")
+                {
+                    result = false;
+                    Console.Write("Goodbye.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That was an invalid entry. Try again.");
+                }
+            }
+            return result;
+        }
+
+    }
+}
